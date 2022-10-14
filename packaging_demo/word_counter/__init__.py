6b64bbabbf4f2eaf47069f972c9d2ff471a36717
Doug <dbergman@sorenson.com>
2022-10-14 12:19:42 -0600

    now importing entire collections package

--- packaging_demo/word_counter/__init__.py
+++ packaging_demo/word_counter/__init__.py
@@ -1,10 +1,10 @@
 # Python packaging demo
 # Reference: https://www.geeksforgeeks.org/how-to-build-a-python-package/
 
-from collections import Counter
+import collections
 
 
 def count_in_list(mylist: list, word: str):
     # count instances of word in mylist
-    c = Counter(mylist)
+    c = collections.Counter(mylist)
     return c[word]
